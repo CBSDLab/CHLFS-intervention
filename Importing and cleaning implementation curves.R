2dc0ad05904bf60e2bc6fcd17ad4dc2bb33cf43c
psh39 <psh39@case.edu>
2025-06-04 17:20:35 -0400

    Added Bass diffusion structure
    Added Bass diffusion model to represent implementation of CHL intervention

---
+++ Importing and cleaning implementation curves.R
@@ -0,0 +1,6 @@
+## Importing and restructuring implementation curves
+## Created by: Peter S. Hovmand June 4, 2025
+## Revised by: 
+
+library(readr)
+implementation <- read_csv("Data/implementation.csv")
